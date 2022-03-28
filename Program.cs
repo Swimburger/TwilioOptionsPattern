47628a25aba2004ab832a71c5beb38f0b9614b26
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-03-28 16:58:34 -0400

    Use environment variables to send SMS with Twilio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+var twilioAccountSid = Environment.GetEnvironmentVariable("TwilioAccountSid");
+var twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+var twilioPhoneNumber = Environment.GetEnvironmentVariable("TwilioPhoneNumber");
+var toPhoneNumber = Environment.GetEnvironmentVariable("ToPhoneNumber");
+
+TwilioClient.Init(twilioAccountSid, twilioAuthToken);
+
+MessageResource.Create(
+	from: new PhoneNumber(twilioPhoneNumber),
+	to: new PhoneNumber(toPhoneNumber),
+	body: "Ahoy!"
+);
