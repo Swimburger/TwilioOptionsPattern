cee0c916d90cc072e607fa9511cadc396947a9cd
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-03-29 19:45:07 -0400

    Add configuration builder

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
-﻿using Twilio;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
-var twilioAccountSid = Environment.GetEnvironmentVariable("TwilioAccountSid");
-var twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
-var fromPhoneNumber = Environment.GetEnvironmentVariable("FromPhoneNumber");
-var toPhoneNumber = Environment.GetEnvironmentVariable("ToPhoneNumber");
+IConfiguration config = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+	.AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true, reloadOnChange: false)
+	.AddEnvironmentVariables()
+	.AddCommandLine(args)
+	.Build();
+
+var twilioAccountSid = config["Twilio:AccountSid"];
+var twilioAuthToken = config["Twilio:AuthToken"];
+
+var fromPhoneNumber = config["Message:From"];
+var toPhoneNumber = config["Message:To"];
+var messageBody = config["Message:Body"];
 
 TwilioClient.Init(
-	username: twilioAccountSid, 
+	username: twilioAccountSid,
 	password: twilioAuthToken
 );
 
 MessageResource.Create(
 	from: fromPhoneNumber,
 	to: toPhoneNumber,
-	body: "Ahoy!"
+	body: messageBody
 );
