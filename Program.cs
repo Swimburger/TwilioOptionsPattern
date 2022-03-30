3444a22d9191f61a0d16317c8eb8713a781d091c
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-03-30 19:20:21 -0400

    Add configuration to DI

--- Program.cs
+++ Program.cs
@@ -1,24 +1,45 @@
 ﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
-using IHost host = Host.CreateDefaultBuilder(args).Build();
-var config = host.Services.GetRequiredService<IConfiguration>();
+using IHost host = Host.CreateDefaultBuilder(args)
+	.ConfigureServices((context, services) =>
+	{
+		services.Configure<TwilioOptions>(context.Configuration.GetSection("Twilio"));
+		services.Configure<MessageOptions>(context.Configuration.GetSection("Message"));
+		services.AddTransient<MessageSender>();
+	})
+	.Build();
 
-var twilioOptions = config.GetSection("Twilio").Get<TwilioOptions>();
+var twilioOptions = host.Services.GetRequiredService<IOptions<TwilioOptions>>().Value;
 TwilioClient.Init(
 	username: twilioOptions.AccountSid,
 	password: twilioOptions.AuthToken
 );
 
-var messageOptions = config.GetSection("Message").Get<MessageOptions>();
-MessageResource.Create(
-	from: messageOptions.From,
-	to: messageOptions.To,
-	body: messageOptions.Body
-);
+var messageSender = host.Services.GetRequiredService<MessageSender>();
+messageSender.SendMessage();
+
+public class MessageSender
+{
+	private readonly MessageOptions messageOptions;
+
+	public MessageSender(IOptions<MessageOptions>  messageOptions)
+	{
+		this.messageOptions = messageOptions.Value;
+	}
+
+	public void SendMessage()
+	{
+		MessageResource.Create(
+			from: messageOptions.From,
+			to: messageOptions.To,
+			body: messageOptions.Body
+		);
+	}
+}
 
 public class TwilioOptions
 {
